70a0cf128e343a4cc55a2240a80734c92b763d68
Matthew <matthew.97.clark@gmail.com>
2019-11-05 19:25:13 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+def reverse_each_word(string)
+  new_array = string.split()
+  new_array.each do |word|
+    word.reverse!
+  end
+  new_array.join(" ")
+end
+
+def reverse_each_word(string)
+  new_array = string.split()
+  new_array.collect do |word|
+    word.reverse!
+  end
+  new_array.join(" ")
+end
